d4215aa97bfe3525b93271bddc7e09604fc5b2e6
Andrian Kozynets <35635242+Aleryp@users.noreply.github.com>
2022-06-29 10:28:35 +0300

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,13 +42,13 @@ resource "aws_security_group" "main" {
     {
       cidr_blocks      = ["0.0.0.0/0", ]
       description      = ""
-      from_port        = 80
+      from_port        = 0
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       protocol         = "-1"
       security_groups  = []
       self             = false
-      to_port          = 80
+      to_port          = 0
     }
   ]
   ingress = [
@@ -61,7 +61,7 @@ resource "aws_security_group" "main" {
       protocol         = "tcp"
       security_groups  = []
       self             = false
-      to_port          = 80
+      to_port          = 8000
     }
   ]
 }
