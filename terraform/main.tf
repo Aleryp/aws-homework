af303ff2df1a4692e91e26511ea3cde2a036beed
Andrian Kozynets <35635242+Aleryp@users.noreply.github.com>
2022-06-29 10:25:13 +0300

    Updated VPC rules in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,13 +42,13 @@ resource "aws_security_group" "main" {
     {
       cidr_blocks      = ["0.0.0.0/0", ]
       description      = ""
-      from_port        = 0
+      from_port        = 80
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       protocol         = "-1"
       security_groups  = []
       self             = false
-      to_port          = 0
+      to_port          = 80
     }
   ]
   ingress = [
