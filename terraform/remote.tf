0be07918b83d45f59e9429d7aa0140c0c57d61c7
Andrian Kozynets <35635242+Aleryp@users.noreply.github.com>
2022-06-29 11:48:58 +0300

    Updated commands

--- terraform/remote.tf
+++ terraform/remote.tf
@@ -13,13 +13,21 @@ connection {
 
 provisioner "remote-exec" {
          inline = [
-                       "sudo apt update",
-                       "sudo apt install python --yes",
-                       "sudo git copy https://github.com/Aleryp/aws-homework.git",
-                       "sudo cd aws-homework/covidfore",
-                       "sudo pip install --upgrade pip",
-                       "sudo pip install -r requirements.txt",
-                       "sudo python run.py"
+                       "sudo apt-get update",
+                       "sudo apt-get install \
+                       ca-certificates \
+                       curl \
+                       gnupg \
+                       lsb-release",
+                       "sudo mkdir -p /etc/apt/keyrings",
+                       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
+                       "echo \
+                       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+                       $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+                       "sudo apt-get update",
+                       "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin",
+                       "sudo docker pull 18273456/covidstat",
+                       "sudo docker run -p 80:80 covidstat",
                   ]
   }
 }
